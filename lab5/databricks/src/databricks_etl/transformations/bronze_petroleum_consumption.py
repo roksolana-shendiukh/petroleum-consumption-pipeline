241433ecd7fe54211015d2e89e54518e81317415
roksolana.shendiu770@softserve.academy <roksolana.shendiu770@softserve.academy>
2026-09-23 13:19:57 +0000

    fix(lab5): fix schema config key and harden consumption bronze autoloader

--- lab5/databricks/src/databricks_etl/transformations/bronze_petroleum_consumption.py
+++ lab5/databricks/src/databricks_etl/transformations/bronze_petroleum_consumption.py
@@ -2,12 +2,13 @@ from pyspark import pipelines as dp
 from pyspark.sql.functions import col, current_timestamp
 
 CONSUMPTION_PATH = spark.conf.get("petroleum.consumption_path")
-SCHEMA_PATH = spark.conf.get("petroleum.schema_path") 
+SCHEMA_LOCATION = spark.conf.get("petroleum.consumption_schema_location")
+
 
 @dp.table(
     name="petroleum_consumption_raw_ldp_bronze",
     table_properties={
-        "pipelines.reset.allowed": "false",   
+        "pipelines.reset.allowed": "false",
         "delta.appendOnly": "true"
     }
 )
@@ -16,8 +17,9 @@ def petroleum_consumption_raw_ldp_bronze():
         spark.readStream
             .format("cloudFiles")
             .option("cloudFiles.format", "json")
-            .option("cloudFiles.schemaLocation", SCHEMA_PATH)
-            .option("cloudFiles.schemaEvolutionMode", "addNewColumns")
+            .option("cloudFiles.schemaLocation", SCHEMA_LOCATION)
+            .option("cloudFiles.schemaEvolutionMode", "rescue")
+            .option("cloudFiles.inferColumnTypes", "false")
             .option("multiLine", "true")
             .load(CONSUMPTION_PATH)
             .withColumn("_source_filename", col("_metadata.file_path"))
