536ff1be91a1736dc868b3a10b593d30e61ea423
roksolana.shendiu770@softserve.academy <roksolana.shendiu770@softserve.academy>
2026-09-23 12:53:02 +0000

    feat(lab5): migrate petroleum consumption bronze to Auto Loader

--- lab5/databricks/src/databricks_etl/transformations/bronze_petroleum_consumption.py
+++ lab5/databricks/src/databricks_etl/transformations/bronze_petroleum_consumption.py
@@ -2,18 +2,22 @@ from pyspark import pipelines as dp
 from pyspark.sql.functions import col, current_timestamp
 
 CONSUMPTION_PATH = spark.conf.get("petroleum.consumption_path")
+SCHEMA_PATH = spark.conf.get("petroleum.schema_path") 
 
-
-@dp.materialized_view(
+@dp.table(
     name="petroleum_consumption_raw_ldp_bronze",
     table_properties={
-        "pipelines.reset.allowed": "true"
+        "pipelines.reset.allowed": "false",   
+        "delta.appendOnly": "true"
     }
 )
 def petroleum_consumption_raw_ldp_bronze():
     return (
-        spark.read
-            .format("json")
+        spark.readStream
+            .format("cloudFiles")
+            .option("cloudFiles.format", "json")
+            .option("cloudFiles.schemaLocation", SCHEMA_PATH)
+            .option("cloudFiles.schemaEvolutionMode", "addNewColumns")
             .option("multiLine", "true")
             .load(CONSUMPTION_PATH)
             .withColumn("_source_filename", col("_metadata.file_path"))
